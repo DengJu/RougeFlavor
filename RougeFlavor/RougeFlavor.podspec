a195aaafad2ae74054ae5f88f07f21d07d58e062
oneselfGuess <xebdison@163.com>
2022-04-14 16:57:29 +0800

    Modyfile open file

--- RougeFlavor/RougeFlavor.podspec
+++ RougeFlavor/RougeFlavor.podspec
@@ -2,14 +2,14 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "RougeFlavor"
-  spec.version      = "1.0.1"
+  spec.version      = "1.0.2"
   spec.summary      = "oneselfGuess"
   spec.description  = "Network Request For MiMo."
   spec.homepage     = "https://github.com/DengJu/RougeFlavor/tree/1.0.0"
   spec.license      = "MIT"
   spec.author             = { "oneselfGuess" => "xebdison@163.com" }
   spec.platform     = :ios, "13.0"
-  spec.pod_target_xcconfig = { 'SWIFT_VERSION' => '5' }
+  spec.swift_version = '5.0'
   spec.source       = { :git => "https://github.com/DengJu/RougeFlavor.git", :tag => "#{spec.version}" }
   spec.source_files  = "RougeFlavor", "RougeFlavor/**/*.{h,m,swift}"
   
---
+++ RougeFlavor/RougeFlavor/RougeRequestData.swift
@@ -0,0 +1,210 @@
+
+import UIKit
+
+public extension RougeRequest {
+    
+    /// 不带图片数据请求
+    /// - Returns: 返回DataRequest
+    private func requestData<T>(_ requestConfig: RougeConfiguration!, success: @escaping (_ data : T)->(), failure: ((ResponseErrorInfo) ->Void)? = nil, uploadProgress: ((Double) -> Void)? = nil) -> DataRequest
+    {
+        let request = AF.request(requestConfig.request)
+        if requestConfig.isEncryptRequest {
+            request.responseString { response in
+                switch response.result {
+                case .success(let responseObject):
+                    guard let code = response.response?.statusCode else { return }
+                    guard code == 200 else {
+                        guard let fail = failure else {
+                            let errorInfo = ResponseErrorInfo(.customError(code, "请求错误："+responseObject), requestConfig.request)
+                            RequestParameterConfig.shared.errorInfo = errorInfo
+                            if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                closure(errorInfo)
+                            }
+                            debugPrint(errorInfo)
+                            return
+                        }
+                        fail(ResponseErrorInfo(.customError(code, responseObject), requestConfig.request))
+                        return
+                    }
+                    guard responseObject.count > 0 else {
+                        let errorInfo = ResponseErrorInfo(.customError(51001, "请求错误：响应为空"), requestConfig.request)
+                        RequestParameterConfig.shared.errorInfo = errorInfo
+                        if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                            closure(errorInfo)
+                        }
+                        debugPrint(errorInfo)
+                        return
+                    }
+                    guard let x_sign = (response.response?.allHeaderFields["x-sign"] ?? response.response?.allHeaderFields["X-Sign"]) as? String else {
+                        let errorInfo = ResponseErrorInfo(.customError(51001, "请求错误：响应头里面没有x-sign参数，可能是请求头参数错误造成"), requestConfig.request)
+                        RequestParameterConfig.shared.errorInfo = errorInfo
+                        if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                            closure(errorInfo)
+                        }
+                        debugPrint(errorInfo)
+                        return
+                    }
+                    if let baseStr = response.value,
+                       let data = Data(base64Encoded: baseStr, options: .ignoreUnknownCharacters),
+                       let decodeArrayData = try? AES(key: requestConfig.DECRYPTKEY.bytes, blockMode: ECB(), padding: .pkcs7).decrypt(data.bytes),
+                       let decodeString = String(data: Data(decodeArrayData), encoding: .utf8),
+                       decodeString.HMAC(key: requestConfig.PUBLICKEY) == x_sign {
+                        if let model = RequestModel<T>.deserialize(from: decodeString.convertDictionary()) {
+                            guard let data = model.data, model.code == 0 else {
+                                guard let fail = failure else {
+                                    let errorInfo = ResponseErrorInfo(.customError(51001, model.msg), requestConfig.request)
+                                    RequestParameterConfig.shared.errorInfo = errorInfo
+                                    if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                        closure(errorInfo)
+                                    }
+                                    debugPrint(errorInfo)
+                                    return
+                                }
+                                fail(ResponseErrorInfo(.customError(model.code, model.msg), requestConfig.request))
+                                return
+                            }
+                            if case .DeleteAccount = requestConfig  {
+                                /// 如果删除账号成功，移除token
+                                RequestParameterConfig.shared.requestToken = ""
+                            }
+                            success(data)
+                        }
+                    }else {
+                        guard let fail = failure else {
+                            let errorInfo = ResponseErrorInfo(.customError(51001, "请求错误：" + "Request failure!"), requestConfig.request)
+                            RequestParameterConfig.shared.errorInfo = errorInfo
+                            if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                closure(errorInfo)
+                            }
+                            debugPrint(errorInfo)
+                            return
+                        }
+                        fail(ResponseErrorInfo(.customError(21001, "解密验签失败"), requestConfig.request))
+                    }
+                case .failure(let error):
+                    if let underlyingError = error.underlyingError {
+                        if let urlError = underlyingError as? URLError {
+                            if urlError.code == .timedOut {
+                                AF.cancelAllRequests()
+                            }else {
+                                let statusCode = urlError.code.rawValue
+                                let errorDesc = urlError.localizedDescription
+                                guard let fail = failure else {
+                                    let errorInfo = ResponseErrorInfo(.customError(51001, errorDesc), requestConfig.request)
+                                    RequestParameterConfig.shared.errorInfo = errorInfo
+                                    if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                        closure(errorInfo)
+                                    }
+                                    debugPrint(errorInfo)
+                                    return
+                                }
+                                fail(ResponseErrorInfo(.httpError(statusCode, errorDesc), requestConfig.request))
+                            }
+                        }
+                    }
+                }
+            }
+        }else {
+            request.responseJSON { response in
+                switch response.result {
+                case .success(_):
+                    guard response.value != nil,
+                          let dict = response.value as? [String: Any],
+                          let model = RequestModel<T>.deserialize(from: dict) else { return }
+                    guard let data = model.data, model.code == 0 else {
+                        guard let fail = failure else {
+                            let errorInfo = ResponseErrorInfo(.customError(51001, model.msg), requestConfig.request)
+                            RequestParameterConfig.shared.errorInfo = errorInfo
+                            if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                closure(errorInfo)
+                            }
+                            debugPrint(errorInfo)
+                            return
+                        }
+                        fail(ResponseErrorInfo(.httpError(model.code, model.msg), requestConfig.request))
+                        return
+                    }
+                    success(data)
+                case .failure(let error):
+                    if let underlyingError = error.underlyingError {
+                        if let urlError = underlyingError as? URLError {
+                            if urlError.code == .timedOut {
+                                AF.cancelAllRequests()
+                            }else {
+                                let statusCode = urlError.code.rawValue
+                                let errorDesc = urlError.localizedDescription
+                                guard let fail = failure else {
+                                    let errorInfo = ResponseErrorInfo(.customError(51001, errorDesc), requestConfig.request)
+                                    RequestParameterConfig.shared.errorInfo = errorInfo
+                                    if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                        closure(errorInfo)
+                                    }
+                                    debugPrint(errorInfo)
+                                    return
+                                }
+                                fail(ResponseErrorInfo(.httpError(statusCode, errorDesc), requestConfig.request))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return request
+    }
+    
+    /// 带图片数据请求
+    /// - Returns: 返回DataRequest
+    private func requestImageData<T>(_ requestConfig: RougeConfiguration, image: UIImage?, success: @escaping (_ data : T)->(), failure: ((ResponseErrorInfo) ->Void)? = nil, uploadProgress: ((Double) -> Void)? = nil) -> DataRequest {
+        let request = AF.upload(multipartFormData: { multipartforData in
+            if let data = image?.jpegData(compressionQuality: 0.5) {
+                multipartforData.append(data, withName: "file", fileName: "avatar.jpg", mimeType: "image/jpeg")
+            }
+        }, with: requestConfig.request)
+        request.responseJSON { response in
+            switch response.result {
+            case .success(_):
+                guard response.value != nil,
+                      let dict = response.value as? [String: Any],
+                      let model = RequestModel<T>.deserialize(from: dict) else { return }
+                guard let data = model.data, model.code == 0 else {
+                    guard let fail = failure else {
+                        let errorInfo = ResponseErrorInfo(.customError(51001, model.msg), requestConfig.request)
+                        RequestParameterConfig.shared.errorInfo = errorInfo
+                        if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                            closure(errorInfo)
+                        }
+                        debugPrint(errorInfo)
+                        return
+                    }
+                    fail(ResponseErrorInfo(.httpError(model.code, model.msg), requestConfig.request))
+                    return
+                }
+                success(data)
+            case .failure(let error):
+                if let underlyingError = error.underlyingError {
+                    if let urlError = underlyingError as? URLError {
+                        if urlError.code == .timedOut {
+                            AF.cancelAllRequests()
+                        }else {
+                            let statusCode = urlError.code.rawValue
+                            let errorDesc = urlError.localizedDescription
+                            guard let fail = failure else {
+                                let errorInfo = ResponseErrorInfo(.customError(51001, errorDesc), requestConfig.request)
+                                RequestParameterConfig.shared.errorInfo = errorInfo
+                                if let closure = RequestParameterConfig.shared.requestErrorInfo {
+                                    closure(errorInfo)
+                                }
+                                debugPrint(errorInfo)
+                                return
+                            }
+                            fail(ResponseErrorInfo(.httpError(statusCode, errorDesc), requestConfig.request))
+                        }
+                    }
+                }
+            }
+        }
+        return request
+    }
+    
+}
+
